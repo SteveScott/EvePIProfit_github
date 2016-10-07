d54ab59474dc4c72750b9c8bb5bca81c2da79443
Steve Scott <stevescott517@gmail.com>
2016-10-07 14:30:30 -0400

    adding dynamic content with jinja template

--- controller.py
+++ controller.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', table = "Table goes here")
 
 if __name__ == "__main__":
     app.run(debug=True)
