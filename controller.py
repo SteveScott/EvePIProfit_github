f6e46f7f6ffeaa9065a8266d35d90e885ae12593
SteveScott <stevescott517@gmail.com>
2016-10-07 18:34:44 -0400

    got templates working for all pages

--- controller.py
+++ controller.py
@@ -6,6 +6,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', table = "Table goes here")
 
+@app.route('/jita')
+def jita():
+	return render_template('jita.html')
+
+@app.route('/amarr')
+def amarr():
+	return render_template('amarr.html')
+
+@app.route('/rens')
+def rens():
+	return render_template('rens.html')
+
+@app.route('/oursulaert')
+def oursulaert():
+	return render_template('oursulaert.html')
+
+@app.route('/dodixie')
+def dodixie():
+	return render_template('dodixie.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
     app.run(debug=True)
