ffb52895143ac007ea2689c9dcde15e627324fe7
Steve Scott <stevescott517@gmail.com>
2017-01-05 19:05:48 -0500

    with help

--- controller.py
+++ controller.py
@@ -1,8 +1,22 @@
 import os, flask, sqlalchemy
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+import urlparse
 
 os.environ['DATABASE_URL']='postgres://lojyjajvpwaaci:4ya_0u6olTZ2taL68me6Goa1HD@ec2-54-243-199-161.compute-1.amazonaws.com:5432/deaek2i6u7a13g'
+
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+con = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -15,7 +29,10 @@ def index():
 
 @app.route('/jita')
 def jita():
-	return render_template('jita.html')
+	cur = con.cursor()
+	cur.execute("SELECT * FROM temp_jita")
+	entries = cur.fetchall()
+	return render_template('jita.html',entries)
 
 @app.route('/amarr')
 def amarr():
@@ -34,5 +51,5 @@ def dodixie():
 	return render_template('dodixie.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(debug=True)
+    app.run(debug=True, port=os.environ.get('PORT', '8080'))
+    #app.run(debug=True)
