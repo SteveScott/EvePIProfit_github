9e43dfbf1c9398c990b2eea1ed0f05b061f2e9ce
Steve Scott <stevescott517@gmail.com>
2017-01-07 17:08:00 -0500

    fix round

--- controller.py
+++ controller.py
@@ -31,7 +31,7 @@ def index():
 def jita():
     cur = con.cursor()
     #cur.execute("SELECT * FROM temp_jita")
-    cur.execute("SELECT name, price, profit, ROUND(profitmargin, 2), mytime FROM name, temp_jita WHERE itemid = id")
+    cur.execute("SELECT name, price, profit, ROUND(profitmargin), mytime FROM name, temp_jita WHERE itemid = id")
     entries = cur.fetchall()
     return render_template('jita.html',entries=entries)
 
