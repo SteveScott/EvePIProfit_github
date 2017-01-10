24d59c0a81c5fe5542353d8323aca68f045a8824
Steve Scott <stevescott517@gmail.com>
2017-01-10 10:20:58 -0500

    added comma to javascript

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('#mainTable').DataTable( {
         "paging":   true,
         "ordering": true,
-        "info":     true
+        "info":     true,
         "order": [[4, "asc"]]
     } );
 } );
