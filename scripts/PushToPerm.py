a1def9870575e046506b08b0f385d5d281ae667b
SteveScott <stevescott517@gmail.com>
2017-07-07 21:39:14 -0400

    remove .env

--- scripts/PushToPerm.py
+++ scripts/PushToPerm.py
@@ -5,17 +5,19 @@ import psycopg2
 import os
 import eveLists
 import connection
+import boto
 
-os.environ['DATABASE_URL']='postgres://lojyjajvpwaaci:4ya_0u6olTZ2taL68me6Goa1HD@ec2-54-243-199-161.compute-1.amazonaws.com:5432/deaek2i6u7a13g'
+
+
 parse.uses_netloc.append("postgres")
 url = parse.urlparse(os.environ["DATABASE_URL"])
 
 con = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
+    database=os.environ['URI'],
+    user=os.environ["DATABASE_USER"],
+    password=os.environ["DATABASE_PASSWORD"],
+    host=os.environ["DATABASE_HOST"],
+    port=os.environ["PORT"]
 )
 
 def main():
