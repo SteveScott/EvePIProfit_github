9b2dd8a521b9a610902e64893f4aaf9d3e376009
SteveScott <stevescott517@gmail.com>
2018-05-13 13:01:01 -0400

    add redis and everything else

--- scripts/clock.py
+++ scripts/clock.py
@@ -21,7 +21,7 @@ from worker import conn
 q=Queue(connection=conn)
 
 
-#'''
+'''
 @scheduler.scheduled_job('cron', hour='0,2,4,6,8,10,12,14,16,18,20,22')
 def clock_scheduled_commands():
     print('Updating Tables')
@@ -29,8 +29,8 @@ def clock_scheduled_commands():
     print('Updating Margins')
     job = q.enqueue_call(func=calculateMargins.main, timeout='6m')
     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
-#'''
 '''
+#'''
 @scheduler.scheduled_job('interval', minutes=5)
 def timed_job():
     #con = connection.establish_connection()
@@ -40,12 +40,10 @@ def timed_job():
     print('Calculating Margins')
     job = q.enqueue_call(func=calculateMargins.main, timeout='6m')
     print(job.get_id())
-    #print('Pushing to Perm')
-    #PushToPerm.main()
     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
     #con.close()
     return 0
-'''
+#'''
 '''
 scheduler.add_job(
     func=print_date_time, # your function here
